00c6b9b6356d61de060c05174997ed46a2381442
unknown <Dustin.Guillen14@yahoo.com>
2020-05-09 21:48:18 -0600

    added server.post promise

--- index.js
+++ index.js
@@ -5,7 +5,49 @@ const server = express();
 
 let users = [];
 
+let hikers = [
+    {
+        "id": shortid.generate(),
+        "name": "Seven",
+        "bio": "Not Tarzan's Wife, another Jane"
+    },
+    {
+        "id": shortid.generate(),
+        "name": "Chinaman",
+        "bio": "Not Tarzan's Wife, another Jane"
+    },
+];
+
 server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const newHiker = req.body;
+    const { name, bio } = req.body;
+
+    if (!name || !bio){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else if (name || bio) {
+        newHiker.id = shortid.generate();
+        hikers.push(newHiker);
+        res.status(201).json(newHiker);
+    } else {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+
+
+    // const newUser = req.body;
+    // newUser.id = shortid.generate();
+    // hikers.push(newUser);
+    // res.status(201).json(newUser);
+
+})
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json(hikers)
+})
+
+
+
 
 const PORT = 420;
 
